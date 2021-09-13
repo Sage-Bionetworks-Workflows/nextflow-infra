29541fcc0c42f5748e787a4a3caefaae49469b93
Bruno Grande <bruno.grande@sagebase.org>
2021-09-13 09:44:49 -0400

    Test new `validate_config()` function

--- bin/test_configure_tower.py
+++ bin/test_configure_tower.py
@@ -36,22 +36,10 @@ class TestProjects:
     # Generate lists of valid and invalid strings
     valid_project_names = ["foo-project", "bar-project"]
     valid_project_filenames = [f"{x}.yaml" for x in valid_project_names]
-    valid_project_filepaths = [os.path.join(CONFIG_DIR, x) for x in valid_project_names]
-    valid_emails = [
-        "bruno.grande@sagebase.org",
-        "tess.thyer@sagebase.org",
-        "thomas.yu@sagebase.org",
-    ]
-    valid_arn_prefixes = [
-        "arn:aws:sts::563295687221:assumed-role/AWSReservedSSO_Viewer_19d3ce703c9acf2e",
-        "arn:aws:sts::563295687221:assumed-role/AWSReservedSSO_Developer_baa6fed639faf5e7",
-        "arn:aws:sts::563295687221:assumed-role/AWSReservedSSO_Administrator_934edbf73c05264e",
-    ]
-    valid_arns = list(map("/".join, zip(valid_arn_prefixes, valid_emails)))
-    invalid_arns = [
-        "arn:aws:iam::035458030717:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_Administrator_580e9f32ac55c4e7",
-        "arn:aws:sts::035458030717:assumed-role/AWSReservedSSO_Viewer_fd80909e6a51c6e7/thomas.yu",
-        "arn:aws:sts::035458030717:assumed-role/AWSReservedSSO_Viewer_fd80909e6a51c6e7",
+    valid_arns = [
+        "arn:aws:sts::563295687221:assumed-role/AWSReservedSSO_Viewer_19d3ce703c9acf2e/bruno.grande@sagebase.org",
+        "arn:aws:sts::563295687221:assumed-role/AWSReservedSSO_Developer_baa6fed639faf5e7/tess.thyer@sagebase.org",
+        "arn:aws:sts::563295687221:assumed-role/AWSReservedSSO_Administrator_934edbf73c05264e/thomas.yu@sagebase.org",
     ]
 
     # Generate valid projects
@@ -72,13 +60,6 @@ class TestProjects:
     }
     valid_projects = [valid_project_1, valid_project_2]
 
-    # Generate invalid projects from the valid ones
-    invalid_project_1 = deepcopy(valid_project_1)
-    invalid_project_1["parameters"] = {}
-    invalid_project_2 = deepcopy(valid_project_2)
-    del invalid_project_2["stack_name"]
-    invalid_projects = [invalid_project_1, invalid_project_2]
-
     @pytest.fixture
     def projects(self, mocker):
         mocker.patch.object(configure_tower.Projects, "extract_users")
@@ -91,7 +72,7 @@ class TestProjects:
 
     def test_list_projects(self, projects, mocker):
         valid_names = self.valid_project_filenames
-        invalid_names = ["ignore.yaml" + "baz-project.txt"]
+        invalid_names = ["ignore.yaml", "baz-project.txt"]
         return_value = [(CONFIG_DIR, [], valid_names + invalid_names)]
         mocker.patch("os.walk", return_value=return_value)
         expected = [os.path.join(CONFIG_DIR, x) for x in valid_names]
@@ -99,7 +80,29 @@ class TestProjects:
         assert actual == expected
         os.walk.assert_called_once()
 
-    def test_load_projects_valid(self, projects, mocker):
+    def test_validate_config(self, projects):
+        # Create a series of valid and invalid test cases
+        num_invalid = 5
+        valid = self.valid_project_1
+        invalid_projects = [deepcopy(valid) for _ in range(num_invalid)]
+        # Test Case 1: Missing access ARNs
+        del invalid_projects[0]["parameters"]["S3ReadWriteAccessArns"]
+        # Test Case 2: Missing parameters key
+        del invalid_projects[1]["parameters"]
+        # Test Case 3: Wrong template name
+        invalid_projects[2]["template_path"] = "foo"
+        # Test Case 4: Missing template_path key
+        del invalid_projects[3]["template_path"]
+        # Test Case 5: Missing stack_name key
+        del invalid_projects[4]["stack_name"]
+        # Test using all test cases
+        for valid_project in self.valid_projects:
+            projects.validate_config(valid_project)
+        for invalid_project in invalid_projects:
+            with pytest.raises(configure_tower.InvalidTowerProject):
+                projects.validate_config(invalid_project)
+
+    def test_load_projects(self, projects, mocker):
         return_value = iter(self.valid_project_filenames)
         mocker.patch.object(projects, "list_projects", return_value=return_value)
         mocker.patch("builtins.open")
@@ -110,21 +113,9 @@ class TestProjects:
         projects.list_projects.assert_called_once()
         open.assert_called_with(self.valid_project_filenames[-1])
 
-    def test_load_projects_invalid(self, projects, mocker):
-        return_value = iter(self.valid_project_filenames)
-        mocker.patch.object(projects, "list_projects", return_value=return_value)
-        mocker.patch("builtins.open")
-        invalid_projects = [{"foo": "bar"}, {"template_path": "baz"}]
-        mocker.patch("yaml.load", side_effect=iter(invalid_projects))
-        expected = list()
-        actual = list(projects.load_projects())
-        assert actual == expected
-        projects.list_projects.assert_called_once()
-        open.assert_called_with(self.valid_project_filenames[-1])
-
     def test_extract_emails_valid(self, projects):
         input = self.valid_arns
-        expected = self.valid_emails
+        expected = [x.split("/")[-1] for x in input]
         actual = projects.extract_emails(input)
         assert actual == expected
 
@@ -135,11 +126,16 @@ class TestProjects:
         assert actual == expected
 
     def test_extract_emails_invalid(self, projects):
-        for arn in self.invalid_arns:
+        invalid_arns = [
+            "arn:aws:iam::035458030717:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_Administrator_580e9f32ac55c4e7",
+            "arn:aws:sts::035458030717:assumed-role/AWSReservedSSO_Viewer_fd80909e6a51c6e7/thomas.yu",
+            "arn:aws:sts::035458030717:assumed-role/AWSReservedSSO_Viewer_fd80909e6a51c6e7",
+        ]
+        for arn in invalid_arns:
             with pytest.raises(ValueError):
                 projects.extract_emails([arn])
 
-    def test_extract_users_valid(self, mocker):
+    def test_extract_users(self, mocker):
         mocker.patch.object(
             configure_tower.Projects,
             "load_projects",
@@ -160,16 +156,6 @@ class TestProjects:
                 mocker.call(maintainers=[], viewers=["foo"]),
             ]
         )
-
-    def test_extract_users_invalid(self, mocker):
-        for invalid_project in self.invalid_projects:
-            mocker.patch.object(
-                configure_tower.Projects,
-                "load_projects",
-                return_value=iter([invalid_project]),
-            )
-            with pytest.raises(AssertionError):
-                configure_tower.Projects(CONFIG_DIR)
 
 
 class TestAwsClient:
