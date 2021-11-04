15650ca6ca7cffe8d7fd1d09da8681ded5f7f6c7
Bruno Grande <bruno.grande@sagebase.org>
2021-11-04 16:38:42 -0700

    Create public buckets for iGenomes reference files

---
+++ bin/mirror-igenomes.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+prefixes=(
+    "Homo_sapiens/Ensembl/GRCh37"
+    "Homo_sapiens/NCBI/GRCh38"
+    "Mus_musculus/Ensembl/GRCm38"
+    "Homo_sapiens/UCSC/hg38"
+    "Homo_sapiens/UCSC/hg19"
+    "Mus_musculus/UCSC/mm10"
+    "Homo_sapiens/GATK/GRCh37"
+    "Homo_sapiens/GATK/GRCh38"
+)
+
+for prefix in ${prefixes[*]}; do
+    echo "Syncing $prefix..."
+    mkdir -p "./$prefix/" \
+    && aws s3 --no-sign-request --region eu-west-1 sync "s3://ngi-igenomes/igenomes/$prefix/" "./$prefix/" \
+    && aws s3 --region us-east-1 sync "./$prefix/" "s3://sage-igenomes/igenomes/$prefix/" \
+    && rm -r "./$prefix/"
+done
