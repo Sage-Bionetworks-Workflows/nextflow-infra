2dc008ee691d23876c4ca8748c97bd11777ce5ab
Brad Macdonald <bwm03570@gmail.com>
2023-06-07 13:52:39 -0600

    address PR comments

--- bin/mirror-lens-references.sh
+++ bin/mirror-lens-references.sh
@@ -17,16 +17,18 @@ prefixes=(
 
 echo "Downloading LENS reference files"
 mkdir -p "./lens/references"
-wget -P ./lens https://gitlab.com/landscape-of-effective-neoantigens-software/nextflow/modules/tools/lens/-/wikis/uploads/b0d8a24036628bd9e6c860eb531919c3/download_general_references.sh \
+cd ./lens
+wget -P . https://gitlab.com/landscape-of-effective-neoantigens-software/nextflow/modules/tools/lens/-/wikis/uploads/b0d8a24036628bd9e6c860eb531919c3/download_general_references.sh \
     https://gitlab.com/landscape-of-effective-neoantigens-software/nextflow/modules/tools/lens/-/wikis/uploads/2238c7f4af5c5499f298315bf5080ad7/download_human_references.sh \
     https://gitlab.com/landscape-of-effective-neoantigens-software/nextflow/modules/tools/lens/-/wikis/uploads/bf210753e3724edf6db2543eb54c3de4/download_mouse_references.sh
-bash download_general_references.sh ./references
-bash download_human_references.sh ./references
-bash download_mouse_references.sh ./references
+export RAFT_PATH="."
+bash download_general_references.sh .
+bash download_human_references.sh .
+bash download_mouse_references.sh .
 
 for prefix in ${prefixes[*]}; do
     echo "Syncing $prefix..."
     aws s3 --region us-east-1 sync "./references/$prefix/" "s3://sage-igenomes/LENS/$prefix/" --acl public-read
 done
-
+cd ..
 rm -r "./lens"
